745749b1b43c0d376d46c03da02aedbfbf450142
Panampilly Vinay Krishna <40162660+Vinay10031993@users.noreply.github.com>
2018-08-26 12:13:56 +0530

    Update prctise.py

--- prctise.py
+++ prctise.py
@@ -2,19 +2,19 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 import os
 
-bot=ChatBot('Bot')
-bot.set_trainer(ListTrainer)
+boter=ChatBot('Bot')
+boter.set_trainer(ListTrainer)
 
 
-for files in os.listdir('C:/Users/vinay krishna/Desktop/pilot project/ChatBot/english/'):
-	data=open('C:/Users/vinay krishna/Desktop/pilot project/ChatBot/english/'+files,'r').readlines()
-	bot.train(data)
+for i in os.listdir('C:/Users/vinay krishna/Desktop/pilot project/ChatBot/english/'):
+	data=open('C:/Users/vinay krishna/Desktop/pilot project/ChatBot/english/'+i,'r').readlines()
+	boter.train(data)
 
 
 while True:
 	message=input("You: ")
 	if message.strip()!="bye":
-		reply=bot.get_response(message)
+		reply=boter.get_response(message)
 		print('ChatBot:',reply)
 
 	if message.strip()=="bye":
